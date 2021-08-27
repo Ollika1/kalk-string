6f99dff3747406afc563f799522c5b8e1aa37919
Ollika1 <86562121+Ollika1@users.noreply.github.com>
2021-08-27 21:53:59 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Scanner;
+//import java.util.Scanner;
 public class Main {
 	public Main(){
 
